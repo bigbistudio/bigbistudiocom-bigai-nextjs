aa8e3a034023f00ac29299acb92945347dc9af7a
ethan-nd <ethan.nd@bigbistudio.com>
2026-08-23 12:47:50 +0200

    feat(home): edit hero main section content

--- src/bigbistudio/pages/home.view.tsx
+++ src/bigbistudio/pages/home.view.tsx
@@ -19,7 +19,7 @@ const page = homePageData
 
 export function HomeView() {
   return (
-    <main className="relative z-10 flex flex-col items-center justify-center w-full">
+    <main className="relative z-10 flex flex-col items-center justify-center w-full lg:py-10">
       <HeroMainSection {...page.sections.heroMain} />
       <LogosSection {...page.sections.clientLogos} />
       <FeaturesCarouselSection {...page.sections.featuresCarousel} />
--- src/bigbistudio/sections/hero/hero-main.section.tsx
+++ src/bigbistudio/sections/hero/hero-main.section.tsx
@@ -1,6 +1,8 @@
+// External
+import Image from "next/image"
+
 // Internal
 import { ButtonCtaGroup } from "@/bigbistudio/components/ui"
-import { UIDashboard } from "@/bigbistudio/components/graphics"
 
 // Types
 import type { SectionSettings } from "@/bigbistudio/types/common.types"
@@ -14,16 +16,24 @@ export function HeroMainSection({
   return (
     <section
       id={id}
-      className="container flex flex-col gap-6 pt-20 pb-0 lg:pb-20 lg:whitespace-pre-line overflow-hidden scroll-mt-12 xl:scroll-mt-24"
+      className="container flex flex-col lg:flex-row lg:items-center gap-6 py-20 lg:whitespace-pre-line overflow-hidden scroll-mt-12 xl:scroll-mt-24"
     >
-      <h1>{title}</h1>
-      <span className="flex flex-col text-ink-tertiary text-body-large">
-        {subtitle}
-      </span>
-      <div className="flex gap-2.5 mb-8">
-        <ButtonCtaGroup buttons={buttons} />
+      <div className="flex flex-col gap-6">
+        <h1>{title}</h1>
+        <span className="flex flex-col text-ink-tertiary text-body-large">
+          {subtitle}
+        </span>
+        <div className="flex gap-2.5 mb-8">
+          <ButtonCtaGroup buttons={buttons} />
+        </div>
       </div>
-      <UIDashboard />
+      <Image
+        src="/vectors/bigbi-stack-large-vector.svg"
+        alt="bigbi stack vector"
+        width={500}
+        height={400}
+        className="mx-auto"
+      />
     </section>
   )
 }
