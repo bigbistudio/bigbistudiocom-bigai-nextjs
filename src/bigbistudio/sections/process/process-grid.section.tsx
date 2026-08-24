3b55d4712466332f8b5121928a590676b35ff3d6
ethan-nd <ethan.nd@bigbistudio.com>
2026-08-24 13:54:49 +0200

    fix(process-grid): adjust container class for improved layout consistency

--- src/bigbistudio/sections/process/process-grid.section.tsx
+++ src/bigbistudio/sections/process/process-grid.section.tsx
@@ -31,7 +31,7 @@ export function ProcessGridSection({
           </p>
         </div>
       </div>
-      <div className="container px-0 flex flex-col lg:grid lg:grid-cols-2 gap-px bg-line-translucent border-y-[0.9px] border-line-translucent">
+      <div className="container lg:px-0 flex flex-col lg:grid lg:grid-cols-2 gap-px lg:bg-line-translucent border-y-[0.9px] border-line-translucent divide-y lg:divide-y-0 divide-line-translucent">
         {processData.phases.map((item) => {
           const UI = item.ui ? uiGraphicMap[item.ui] : null
 
