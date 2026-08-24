1bfefe0956f8c6ef178a8b9b16685bc0221ddc25
ethan-nd <ethan.nd@bigbistudio.com>
2026-08-24 19:16:49 +0200

    feat(products): add product list page data and metadata

---
+++ src/bigbistudio/data/pages/products/index.ts
@@ -0,0 +1,6 @@
+import type { ProductsPageConfig } from "@/bigbistudio/types/pages.types"
+
+import productsJson from "./products.json"
+
+export const productsPageData =
+    productsJson as ProductsPageConfig
