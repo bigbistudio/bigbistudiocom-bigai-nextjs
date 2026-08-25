05eb46f1967180885829a54bfc13b8aa0e628bf2
ethan-nd <ethan.nd@bigbistudio.com>
2026-08-25 11:35:39 +0200

    fix(product-list): adjust layout for product list view

--- src/bigbistudio/pages/product-list.view.tsx
+++ src/bigbistudio/pages/product-list.view.tsx
@@ -16,12 +16,10 @@ type ProductListViewProps = {
 export function ProductListView({ Products }: ProductListViewProps) {
   return (
     <main className="relative z-10 flex flex-col items-center justify-center w-full">
-      <div className="flex flex-col justify-center border border-line-translucent divide-y divide-line-translucent max-w-full">
+      <div className="flex flex-col justify-center border border-line-translucent divide-y divide-line-translucent container px-0">
         <HeroSimpleSection {...page.sections.heroSimple} />
-        <div className="w-full mx-auto">
-          <div className="container flex flex-col justify-center py-20">
-            <ProductGrid Products={Products} />
-          </div>
+        <div className="container flex flex-col justify-center py-20">
+          <ProductGrid Products={Products} />
         </div>
       </div>
     </main>
