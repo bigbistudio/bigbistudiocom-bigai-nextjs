f46a153cafa5b5aeeb67c1bd87327ba5f85a1f5b
ethan-nd <ethan.nd@bigbistudio.com>
2026-08-27 04:39:24 +0200

    feat(products): add title to product list section

--- src/bigbistudio/pages/product-list.view.tsx
+++ src/bigbistudio/pages/product-list.view.tsx
@@ -6,6 +6,8 @@ import {
 } from "@/bigbistudio/sections"
 import { ProductGrid } from "@/bigbistudio/sections/product-list/grids/product-grid"
 
+import { EyebrowIcon } from "@/bigbistudio/components/icons"
+
 import { productsPageData } from "@/bigbistudio/data/pages"
 import { highlightsData } from "@/bigbistudio/data/highlights"
 
@@ -23,7 +25,13 @@ export function ProductListView({ Products }: ProductListViewProps) {
     <main className="relative z-10 flex flex-col items-center justify-center w-full">
       <div className="flex flex-col justify-center border border-line-translucent divide-y divide-line-translucent container px-0">
         <HeroSimpleSection {...page.sections.heroSimple} />
-        <div className="container flex flex-col justify-center py-20">
+        <div className="container flex flex-col justify-center gap-16 py-20">
+          <div className="flex flex-col lg:items-center gap-2.5">
+            <span className="flex items-center px-1 gap-1.5 text-sm text-ink-tertiary">
+              <EyebrowIcon /> Product List
+            </span>
+            <h2 className="lg:whitespace-pre-line">Available Editions</h2>
+          </div>
           <ProductGrid Products={Products} />
         </div>
         <FeaturesCarouselSection
