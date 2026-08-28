71ab9383eef72691792df94f6524f4ffb62a293f
ethan-nd <ethan.nd@bigbistudio.com>
2026-08-28 12:59:35 +0200

    feat(about): update about page content

--- src/bigbistudio/pages/about.view.tsx
+++ src/bigbistudio/pages/about.view.tsx
@@ -1,11 +1,5 @@
 // Internal
-import {
-  HeroSimpleSection,
-  TeamSection,
-  LogosSection,
-  TestimonialsSection,
-  BlogSection,
-} from "@/bigbistudio/sections"
+import { HeroSimpleSection, LogosSection } from "@/bigbistudio/sections"
 import { PortableText } from "@/bigbistudio/components/ui"
 
 import { aboutPageData } from "@/bigbistudio/data/pages"
@@ -19,14 +13,15 @@ const aboutContent = page.sections.content as PortableBlock[]
 export function AboutView() {
   return (
     <main className="relative z-10 flex flex-col items-center justify-center w-full">
-      <HeroSimpleSection {...page.sections.heroSimple} />
-      <section className="container prose prose-invert flex flex-col text-left xl:max-w-210">
-        <PortableText blocks={aboutContent} />
-      </section>
-      <TeamSection {...page.sections.team} />
-      <LogosSection {...page.sections.clientLogos} />
-      <TestimonialsSection {...page.sections.testimonials} />
-      <BlogSection {...page.sections.blog} />
+      <div className="flex flex-col justify-center border border-line-translucent divide-y divide-line-translucent container px-0">
+        <HeroSimpleSection {...page.sections.heroSimple} />
+        <section className="container flex justify-center py-20">
+          <div className="prose prose-invert flex flex-col text-left [&_strong]:text-brand-surface xl:max-w-210">
+            <PortableText blocks={aboutContent} />
+          </div>
+        </section>
+        <LogosSection {...page.sections.clientLogos} />
+      </div>
     </main>
   )
 }
