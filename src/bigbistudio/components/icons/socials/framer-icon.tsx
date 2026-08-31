a5cef4ea609e8fe764e047105b4c5fca1357a0d7
ethan-nd <ethan.nd@bigbistudio.com>
2026-08-31 11:22:51 +0200

    fix(icons): remove redundant role attribute from SVG icons

--- src/bigbistudio/components/icons/socials/framer-icon.tsx
+++ src/bigbistudio/components/icons/socials/framer-icon.tsx
@@ -5,7 +5,6 @@ export function FramerIcon(props: SVGProps<SVGSVGElement>) {
     <svg
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 24 24"
-      role="img"
       {...props}
       fill="currentColor"
     >
